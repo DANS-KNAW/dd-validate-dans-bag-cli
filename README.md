d90bce9f627e40e9f653931592f2319488393587
Jan van Mansum <janvanmansum@gmail.com>
2024-05-22 15:27:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+dd-validate-dans-bag-cli
+===========
+![Build Status](https://github.com/DANS-KNAW/dd-validate-dans-bag-cli/actions/workflows/build.yml/badge.svg)
+[![codecov](https://codecov.io/gh/DANS-KNAW/dd-validate-dans-bag-cli/branch/master/graph/badge.svg)](https://codecov.io/gh/DANS-KNAW/dd-validate-dans-bag-cli)
+![Site Status](https://github.com/DANS-KNAW/dd-validate-dans-bag-cli/actions/workflows/docs.yml/badge.svg)
+
+For documentation see: https://dans-knaw.github.io/dd-validate-dans-bag-cli
---
+++ src/main/java/nl/knaw/dans/validatecli/DdValidateDansBagCli.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2024 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nl.knaw.dans.validatecli;
+
+import lombok.extern.slf4j.Slf4j;
+import nl.knaw.dans.lib.util.AbstractCommandLineApp;
+import nl.knaw.dans.lib.util.CliVersionProvider;
+import nl.knaw.dans.validatecli.config.DdValidateDansBagCliConfig;
+import picocli.AutoComplete.GenerateCompletion;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name = "dans-bag-validate",
+         mixinStandardHelpOptions = true,
+         versionProvider = CliVersionProvider.class,
+         description = "Command-line client for validating DANS bags")
+@Slf4j
+public class DdValidateDansBagCli extends AbstractCommandLineApp<DdValidateDansBagCliConfig> {
+    public static void main(String[] args) throws Exception {
+        new DdValidateDansBagCli().run(args);
+    }
+
+    public String getName() {
+        return "Command-line client for validating DANS bags";
+    }
+
+    @Override
+    public void configureCommandLine(CommandLine commandLine, DdValidateDansBagCliConfig config) {
+        // TODO: set up the API client, if applicable
+        log.debug("Configuring command line");
+        // TODO: add options and subcommands
+    }
+}
